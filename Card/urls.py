336369307f438f24d57acb15e4cc5dec5b283942
Foofdof <foofdof@gmail.com>
2020-10-13 12:59:49 +0300

    Download page

--- Card/urls.py
+++ Card/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.download, name='download'),
+    path('post_list/', views.post_list, name='post_list'),
     path('feedback/new/', views.fb_new, name='feedback_edit'),
 ]
--- Card/views.py
+++ Card/views.py
@@ -7,6 +7,9 @@ def post_list(request):
     feedbacks = FeedBack.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'card/post_list.html', {'feedbacks': feedbacks})
 
+def download(request):
+    return render(request, 'card/download.html')
+
 def fb_new(request):
     if request.method == "POST":
         form = FeedBackForm(request.POST)
