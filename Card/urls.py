7bfbe8137058709674249d056ee369e5ccc7580b
Foofdof <foofdof@gmail.com>
2020-10-13 19:13:57 +0300

    eximg

--- Card/urls.py
+++ Card/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.download, name='download'),
     path('post_list/', views.post_list, name='post_list'),
-    path('drawing_exe/', views.drawing_exe, name='drawing_exe'),
     path('feedback/new/', views.fb_new, name='feedback_edit'),
 ]
